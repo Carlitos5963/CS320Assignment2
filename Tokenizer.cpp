45dec6bcab44006cf63a958f69175072da94f73c
Jose Carlos Gomez <josecarlosgomez69@gmail.com>
2017-06-15 21:30:55 +0000

    Update Tokenizer.cpp

--- Tokenizer.cpp
+++ Tokenizer.cpp
@@ -1,4 +1,4 @@
-#include "Tokenizer.hpp"
+//#include "Tokenizer.hpp"
 #include <iostream>
 #include <istream>
 #include <iterator>
@@ -11,43 +11,18 @@
 // Namespace to declare scope for the class and functions
 using namespace std;
 
-// Protoype for the method GetToken()
-std::vector<std::string>* GetToken();
 
 // Class for a tokenizer object
 class Tokenizer{
 	public:
-	// Function that takes in a string from STDIN and and splits it up into tokens.
-	// Returns a pointer to a vector of type string (vector<string>*)
-	vector<string> * GetToken()
-	{
-
-		vector<string>* myVector = new vector<string>;
-		char arr[66]; // Array to hold user input
+	vector<string> * GetToken();
+	//Tokenizer();
 	
-		cin.getline(arr, 65); // Used to collect input from user
-		string str(arr); // Converts contents of array into a string
-		istringstream iss(str); // String stream used to break up the string
-	
-		do
-		{
-			string sub; // Token to hold tokens temporarily
-			iss >> sub; // Will take part of string and place it in the token variable
-			myVector->push_back(sub); // Places token into the vector
-		}
-		while(iss); // Will repeat if there is more string
-
-		myVector->pop_back(); // Remove the last element of vector (empty element)
-
-		return myVector;
-
-	}
-
 };
 
 // Function that takes in a string from STDIN and and splits it up into tokens.
 // Returns a pointer to a vector of type string (vector<string>*)
-vector<string> * GetToken()
+vector<string> * Tokenizer::GetToken()
 {
 
 	vector<string>* myVector = new vector<string>;
@@ -75,9 +50,9 @@ vector<string> * GetToken()
 int main(){
 	Tokenizer tok;
 	tok.GetToken();
-	vector<string>* v = GetToken();
-	cout << "Vector size: " << v->size() << endl;
-	for (vector<string>::const_iterator i = v->begin(); i != v->end(); i++)
-cout << *i << ' ';
+	//vector<string>* v = GetToken();
+	//cout << "Vector size: " << v->size() << endl;
+	//for (vector<string>::const_iterator i = v->begin(); i != v->end(); i++)
+//cout << *i << ' ';
 }
 */
