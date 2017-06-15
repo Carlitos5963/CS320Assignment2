a9917f35cf4c6922be8f064a782546098c6f8515
Jose Carlos Gomez <josecarlosgomez69@gmail.com>
2017-06-15 16:04:50 -0700

    Tokenizer commit

--- Tokenizer.cpp
+++ Tokenizer.cpp
@@ -32,8 +32,10 @@ vector<string> * Tokenizer::GetTokens()
 		string sub; // Token to hold tokens temporarily
 		iss >> sub; // Will take part of string and place it in the token variable
 		myVector->push_back(sub); // Places token into the vector
-		int tempNum = string::stoi(sub);
-		if(isdigit(tempNum)){
+
+		int testNum = atoi(sub);
+
+		if(testNum != 0){
 			intCount++;
 			}
 		else{
