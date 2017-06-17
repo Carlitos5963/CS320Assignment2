dbc39ec33236dd8975084cd0612182ea9ad9a93e
Jose Carlos Gomez <josecarlosgomez69@gmail.com>
2017-06-17 14:05:43 -0700

    Another commit

--- Tokenizer.cpp
+++ Tokenizer.cpp
@@ -92,7 +92,7 @@ vector<string> * Tokenizer::GetTokens()
 		}
 	//If there is one string and it is not quit
 	else if(strCount == 2 && quitTrue != 1){
-		myVector = GetTokens();
+		//myVector = GetTokens();
 		return myVector;
 		}
 	else{
