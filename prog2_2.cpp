ddaa306f5c1c227aa78b47680988d5a0c502f9c9
Jose Carlos Gomez <josecarlosgomez69@gmail.com>
2017-06-17 02:03:42 -0700

    Tokenizer commit

---
+++ prog2_2.cpp
@@ -0,0 +1,17 @@
+#include "Tokenizer.hpp"
+#include <stdio.h>
+#include <string>
+#include <ctype.h>
+#include <stdlib.h>
+#include <vector>
+
+
+
+int main(){
+
+	Tokenizer tok;
+	tok.GetTokens();
+
+	return 1;
+}
+
