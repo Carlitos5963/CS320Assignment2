1b890eb36ef4f1d89f5ef8e25e2f81d7c035375c
Jose Carlos Gomez <josecarlosgomez69@gmail.com>
2017-06-16 19:23:50 -0700

    Tokenizer commit

--- Tokenizer.cpp
+++ Tokenizer.cpp
@@ -45,6 +45,7 @@ vector<string> * Tokenizer::GetTokens()
 //Work on this---------------->
 			if((strcasecmp(ptr, "quit\n")) != 0){
 				quitTrue = 1;
+cout << "THIS WILL QUIT! << endl;
 				}
 			}
 	}
