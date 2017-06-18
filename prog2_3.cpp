6a8be1699aeb954a5094da3c51776be5a3481ea4
Jose Carlos Gomez <josecarlosgomez69@gmail.com>
2017-06-17 17:39:31 -0700

    Another commit

--- prog2_3.cpp
+++ prog2_3.cpp
@@ -17,19 +17,16 @@ int main(){
 	cout << "Assignment #2-3, Jose Carlos Gomez, JoseCarlosGomez69@gmail.com" << endl;
 	Tokenizer tok; //Instance of the Token object
 	Stack<string> stack;
-	
+	int count = 0;
 
 	// Set to run until told to quit
 	while(1){
 
+		count++;
 		v = tok.GetTokens(); //Set pointer equal to a vector
 		char *ptr = &v->back()[0];
 
-		//Checks to see if the token says 'quit'
-		if((strcasecmp(ptr, "quit\0") == 0 || strcasecmp(ptr, "quit") == 0)){
-			stack.Print();
-			return 0;
-			}
+
 
 		//If first token says 'pop', then pop top stack token off of stack
 		if((strcmp(ptr, "pop\0") == 0 || strcmp(ptr, "pop") == 0) && v->size() > 1){
@@ -39,6 +36,11 @@ int main(){
 		//If first token says 'push', then push second token into stack
 		if((strcmp(ptr, "push\0") == 0 || strcmp(ptr, "push") == 0)){
 			stack.Push(v->back());
+			return 0;
+			}
+		//Checks to see if the token says 'quit'
+		if((strcasecmp(ptr, "quit\0") == 0 || strcasecmp(ptr, "quit") == 0) || count > 5){
+			stack.Print();
 			return 0;
 			}
 
