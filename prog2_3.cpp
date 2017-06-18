c65c54af3a4bd444ec6830551dc57d3b0a1319c0
Jose Carlos Gomez <josecarlosgomez69@gmail.com>
2017-06-17 17:55:31 -0700

    Another commit

--- prog2_3.cpp
+++ prog2_3.cpp
@@ -35,7 +35,7 @@ int main(){
 			}
 		//If first token says 'push', then push second token into stack
 		if((strcmp(ptr, "push\0") == 0 || strcmp(ptr, "push") == 0)){
-			stack.Push(v[1]);
+			stack.Push(v->back()[0]);
 			return 0;
 			}
 		//Checks to see if the token says 'quit'
